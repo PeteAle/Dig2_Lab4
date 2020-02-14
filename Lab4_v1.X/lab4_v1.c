ec11a9a3bd43c6cd344fd71000b67b32a0b2f341
Pete <yau17914@uvg.edu.gt>
2020-02-14 12:04:43 -0600

    Initial commit

---
+++ Lab4_v1.X/lab4_v1.c
@@ -0,0 +1,15 @@
+/*
+ * File:   lab4_v1.c
+ * Author: Peter
+ *
+ * Created on February 14, 2020, 12:00 PM
+ */
+
+
+#include <xc.h>
+#include "lib_adc.h"
+#include "lib_osccon.h"
+
+void main(void) {
+    return;
+}
