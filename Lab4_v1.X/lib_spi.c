5426c493fcf0ef2eb15b863426b6d800a249cf61
Pete <yau17914@uvg.edu.gt>
2020-02-19 14:16:58 -0600

    Lib_SPI
    Se creó y se modificó la librería del SPI

--- Lab4_v1.X/lib_spi.c
+++ Lab4_v1.X/lib_spi.c
@@ -7,7 +7,46 @@
 
 
 #include <xc.h>
+#include "lib_spi.h"
+#include <stdio.h>
+#include <stdint.h>
 
-void main(void) {
-    return;
+void spi_msinit(uint8_t mode){
+    switch(mode){
+        case 0:                     // Master mode Fosc/4.
+            TRISCbits.TRISC5 = 0;
+            TRISCbits.TRISC3 = 0;
+            SSPSTATbits.SMP = 0;
+            SSPSTATbits.CKE = 0;
+            SSPCON = 0b00100000;
+            //SSPCONbits.CKP = 0;
+            //SSPCONbits.SSPEN = 1;
+            break;
+        case 1:                     // Master mode Fosc/16
+            TRISCbits.TRISC5 = 0;
+            TRISCbits.TRISC3 = 0;
+            SSPSTATbits.SMP = 0;
+            SSPSTATbits.CKE = 0;
+            SSPCON = 0b00100001;
+            //SSPCONbits.CKP = 0;
+            //SSPCONbits.SSPEN = 1;
+            break;
+        case 2:                     // Master mode Fosc/64
+            TRISCbits.TRISC5 = 0;
+            TRISCbits.TRISC3 = 0;
+            SSPSTATbits.SMP = 0;
+            SSPSTATbits.CKE = 0;
+            SSPCON = 0b00100010;
+            //SSPCONbits.CKP = 0;
+            //SSPCONbits.SSPEN = 1;
+            break;
+        case 3:                     // Slave mode, SS enabled.
+            TRISCbits.TRISC5 = 0;
+            TRISCbits.TRISC3 = 1;
+            TRISCbits.TRISC4 = 1;
+            SSPSTATbits.SMP = 0;
+            SSPSTATbits.CKE = 0;
+            SSPCON = 0b00100100;
+            break;
+    }
 }
